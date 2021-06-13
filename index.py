92ed10706002fea87f927e49b14131e3673cc757
= <error1001es@gmail.com>
2021-06-13 13:47:52 +0600

    some fixes

--- index.py
+++ index.py
@@ -10,13 +10,14 @@ my_id = client.get_me().id
 @client.on(events.NewMessage())
 async def normal_handler(event):
 
-    if(event.message.from_id.user_id == my_id and event.message.text == start_command):
+    if(hasattr(event.message.from_id, 'user_id') and event.message.from_id.user_id == my_id and event.message.text == start_command):
         i = 1000
         while i > 0:
             sleep(sleep_time)
             await client.send_message(event.message.chat_id, str(i)+' - 7 = '+str(i-7))
             i -= 7
-        await client.send_message(event.message.chat_id, end_message)
+        if(end_message != ''):
+            await client.send_message(event.message.chat_id, end_message)
 
 client.start()
 client.run_until_disconnected()    
