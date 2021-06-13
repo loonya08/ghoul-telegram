1f592b2e8c3f1a996325a2634126d40f7c16b203
= <error1001es@gmail.com>
2021-06-13 12:56:41 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# Вставляем api_id и api_hash
+api_id = api_id
+api_hash = 'api_hash'
+start_command = 'Я гуль' # Команда активации цикла
+end_message = 'l l let me die' # Сообщение после конца цикла, если не нужно - оставляем пустым
+sleep_time = 0.14286
